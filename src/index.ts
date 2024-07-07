acab97e2309ff9f7696fe75452f03250cf12271f
Suhaib Khan <suhaib0900@gmail.com>
2024-07-07 21:24:50 +0100

    Change in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express } from 'express';
 import serverConfig from './config/serverConfig';
 const app: Express = express();
-const PORT=serverConfig.PORT;
+const PORT = serverConfig.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log('Hello World');
