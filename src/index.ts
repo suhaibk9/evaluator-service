208e52720208827b30e6fe2899e2c2ae3af7e906
Suhaib Khan <suhaib0900@gmail.com>
2024-07-07 20:53:34 +0100

    Initial Setup

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express, { Express } from 'express';
+import serverConfig from './config/serverConfig';
+const app: Express = express();
+const PORT=serverConfig.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log('Hello World');
+});
