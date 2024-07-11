7511cb980c48c889e9b6e19b6351b6e3bc0d001e
Suhaib Khan <suhaib0900@gmail.com>
2024-07-11 02:19:44 +0100

    Fixed Error

--- src/validator/createSubmissionValidator.ts
+++ src/validator/createSubmissionValidator.ts
@@ -2,12 +2,20 @@ import { ZodSchema } from 'zod';
 import { Request, Response, NextFunction } from 'express';
 export const validateCreateSubmissionDto =
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (schema: ZodSchema<any>) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    try {
-      schema.parse({ ...req.body });
-      next();
-    } catch (error) {
-      return res.status(400).json({ message: 'Bad Request' });
-    }
-  };
+
+    (schema: ZodSchema<any>) =>
+    (req: Request, res: Response, next: NextFunction) => {
+      try {
+        schema.parse({ ...req.body });
+        next();
+      } catch (error) {
+        return res
+          .status(400)
+          .json({
+            success: false,
+            message: 'Invalid request params received',
+            data: {},
+            error: error,
+          });
+      }
+    };
